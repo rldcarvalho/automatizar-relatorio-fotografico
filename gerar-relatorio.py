fc771752a66a82d88bbffe0de9b67551f195aa13
Rafael Carvalho <rafa_rafael_lucas@hotmail.com>
2022-05-19 21:38:20 -0300

    Etapas do projeto
    Adicionada breve descrição estruturada com as etapas do projeto

---
+++ gerar-relatorio.py
@@ -0,0 +1,20 @@
+#criar arquivo word
+
+#criar interface para armazenagem de fotos
+
+#inserir foto no arquivo word
+
+#inserir mais de uma foto no arquivo world e formatar corretamente
+
+#criar interface para inserção de coordenadas
+
+#inserir balão formatado na ultima imagem
+
+#inserir mais de um balão formatado na ultima imagem
+
+#---------------------------------------------------------
+
+#criar interface grafica para seleção de tipo de relatório
+
+#criar uma interface para cada tipo de relatório com os campos necessários a serem preenchidos
+
